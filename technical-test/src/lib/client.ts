bb3c07fca09539615c5583e878ad3da9b5d09795
Grégory Le Garec <gregory.legarec@gmail.com>
2019-09-09 10:44:00 +0200

    feat: Add Wecasa client ✨

---
+++ technical-test/src/lib/client.ts
@@ -0,0 +1,29 @@
+export interface Booking {
+  prestations: Array<string>;
+  appointment?: Date;
+  address?: string;
+}
+
+const url = "https://www.wecasa.fr/api/techtest";
+
+export class WecasaClient {
+  async fetchHaircutUniverse() {
+    const response = await fetch(`${url}/universe`, {
+      headers: new Headers({ Accept: "application/json" })
+    });
+    return response.json();
+  }
+
+  async saveBooking(booking: Booking) {
+    return fetch(`${url}/booking`, {
+      method: "POST",
+      headers: new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(booking)
+    });
+  }
+}
+
+export default WecasaClient;
